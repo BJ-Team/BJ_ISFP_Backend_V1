25d1798b89ec16d65a104ea025d71c8cca2c2d27
son <sonjm3518@dsm.hs.kr>
2022-04-11 14:26:10 +0900

    ⚡ :: User 속성 추가

--- src/main/java/com/example/bj_isfp_backend/domain/user/domain/User.java
+++ src/main/java/com/example/bj_isfp_backend/domain/user/domain/User.java
@@ -1,4 +1,53 @@
 package com.example.bj_isfp_backend.domain.user.domain;
 
-public class User {
+import com.example.bj_isfp_backend.domain.user.domain.type.Sex;
+import com.example.bj_isfp_backend.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String user_id;
+
+    @Column(length = 30, nullable = false)
+    private String password;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String place;
+
+    @Enumerated(EnumType.STRING)
+    private Sex sex;
+
+    @Column(name = "device_token")
+    private String deviceToken;
+
+    @Builder
+    public User(String user_id, String password, String name, String place, Sex sex) {
+        this.user_id = user_id;
+        this.password = password;
+        this.name = name;
+        this.place = place;
+        if (sex != null) this.sex = sex;
+    }
 }
