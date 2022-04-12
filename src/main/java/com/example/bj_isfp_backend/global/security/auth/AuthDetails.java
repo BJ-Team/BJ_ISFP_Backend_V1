0b1b159b6b98b5cbeef321abb3ff31784ed89b2d
son <sonjm3518@dsm.hs.kr>
2022-04-12 16:46:24 +0900

    📑 :: Auth Details

---
+++ src/main/java/com/example/bj_isfp_backend/global/security/auth/AuthDetails.java
@@ -0,0 +1,52 @@
+package com.example.bj_isfp_backend.global.security.auth;
+
+import com.example.bj_isfp_backend.domain.user.domain.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@RequiredArgsConstructor
+public class AuthDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUser_id();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
