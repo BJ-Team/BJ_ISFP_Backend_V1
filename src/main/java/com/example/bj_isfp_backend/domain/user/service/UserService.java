6c4fc7cdf034e5a4158bd62e6204c4ae35ef2369
son <sonjm3518@dsm.hs.kr>
2022-04-13 13:51:46 +0900

    ⚡ :: (#19) login 추가

--- src/main/java/com/example/bj_isfp_backend/domain/user/service/UserService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/user/service/UserService.java
@@ -1,7 +1,12 @@
 package com.example.bj_isfp_backend.domain.user.service;
 
+import com.example.bj_isfp_backend.domain.auth.presentation.dto.TokenResponse;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.LoginRequest;
 import com.example.bj_isfp_backend.domain.user.presentation.dto.SignUpRequest;
 
 public interface UserService {
+
     void signUp(SignUpRequest signUpRequest);
+
+    TokenResponse login(LoginRequest loginRequest);
 }
