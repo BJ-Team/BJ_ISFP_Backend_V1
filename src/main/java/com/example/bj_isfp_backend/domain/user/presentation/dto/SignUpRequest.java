8a5e38c57b59c65900cf11894e80f3705a681949
son <sonjm3518@dsm.hs.kr>
2022-04-13 09:29:47 +0900

    📑 :: (#17) SignUp Request

---
+++ src/main/java/com/example/bj_isfp_backend/domain/user/presentation/dto/SignUpRequest.java
@@ -0,0 +1,29 @@
+package com.example.bj_isfp_backend.domain.user.presentation.dto;
+
+import com.example.bj_isfp_backend.domain.user.domain.type.Sex;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@NoArgsConstructor
+public class SignUpRequest {
+
+    @NotBlank(message = "user_id는 필수 입력입니다.")
+    private String user_id;
+
+    @NotBlank(message = "password는 필수 입력입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}$",
+            message = "비밀번호는 영문자와 숫자, 특수문자를 모두 포함해서 8~16자리 이내로 입력해주세요.")
+    private String password;
+
+    @NotBlank(message = "name은 필수 입력입니다.")
+    private String name;
+
+    @NotBlank(message = "place는 필수 입력입니다.")
+    private String place;
+
+    private Sex sex;
+}
