88ef97014ad1aea6948a2962b9ff2fd3614385f4
son <sonjm3518@dsm.hs.kr>
2022-04-13 13:15:45 +0900

    ⚡ :: (#19) Login Service Implement 추가

--- src/main/java/com/example/bj_isfp_backend/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/example/bj_isfp_backend/domain/user/service/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.example.bj_isfp_backend.domain.user.service;
 
+import com.example.bj_isfp_backend.domain.auth.exception.PasswordNotCorrectException;
+import com.example.bj_isfp_backend.domain.auth.presentation.dto.TokenResponse;
 import com.example.bj_isfp_backend.domain.user.domain.User;
 import com.example.bj_isfp_backend.domain.user.domain.repository.UserRepository;
 import com.example.bj_isfp_backend.domain.user.facade.UserFacade;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.LoginRequest;
 import com.example.bj_isfp_backend.domain.user.presentation.dto.SignUpRequest;
+import com.example.bj_isfp_backend.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,8 +20,10 @@ public class UserServiceImpl implements UserService {
     private final UserFacade userFacade;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
+    @Transactional
     public void signUp(SignUpRequest signUpRequest) {
 
         userFacade.nameAlreadyExists(signUpRequest.getName());
@@ -29,5 +36,26 @@ public class UserServiceImpl implements UserService {
                         .place(signUpRequest.getPlace())
                         .sex(signUpRequest.getSex())
                         .build());
+    }
+
+    @Override
+    @Transactional
+    public TokenResponse login(LoginRequest loginRequest) {
+
+        User user = userFacade.getByUser_id(loginRequest.getUser_id());
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword()))
+            throw PasswordNotCorrectException.EXCEPTION;
+
+        user.setDeviceToken(loginRequest.getDeviceToken());
+
+        String access = jwtTokenProvider.generateAccessToken(loginRequest.getUser_id());
+        String refresh = jwtTokenProvider.generateRefreshToken(loginRequest.getUser_id());
+
+        return TokenResponse.builder()
+                .accessToken(access)
+                .expired_at(jwtTokenProvider.getExpiredTime())
+                .refreshToken(refresh)
+                .build();
     }
 }
