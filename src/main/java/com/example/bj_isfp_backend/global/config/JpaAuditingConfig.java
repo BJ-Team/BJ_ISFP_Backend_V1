cb34427a882f7327e25aa956d99b138d513cc951
son <sonjm3518@dsm.hs.kr>
2022-04-13 21:08:49 +0900

    📑 :: (#25) Jpa Auditing Config

---
+++ src/main/java/com/example/bj_isfp_backend/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example.bj_isfp_backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
