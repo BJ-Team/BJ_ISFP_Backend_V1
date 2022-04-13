686a9ca07351c4a3f496fd560cb46bf6a2d9fc28
son <sonjm3518@dsm.hs.kr>
2022-04-13 12:28:16 +0900

    ⚡ :: (#19) Password Not Correct 추가

--- src/main/java/com/example/bj_isfp_backend/global/error/ErrorCode.java
+++ src/main/java/com/example/bj_isfp_backend/global/error/ErrorCode.java
@@ -11,8 +11,12 @@ public enum ErrorCode {
 
     BAD_REQUEST(400, "CLIENT-400-1", "Bad Request"),
 
+    PASSWORD_NOT_CORRECT(409, "AUTH-409-1", "Password Not Correct"),
+
     USER_NOT_FOUND(404, "USER-404-1", "User Not Found"),
-    USER_NAME_ALREADY_EXISTS(409, "User-409-1", "User Name1`d7g6f Already Exists"),
+    USER_NAME_ALREADY_EXISTS(409, "USER-409-1", "User Name Already Exists"),
+
+
 
     EXPIRED_JWT(401, "JWT-401-1", "Expired Jwt"),
     INVALID_JWT(401, "JWT-401-2", "Invalid Jwt");
