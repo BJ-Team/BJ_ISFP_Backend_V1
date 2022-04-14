2fd3658d6d3e595cf6475afad440c3d21b8d2db5
son <sonjm3518@dsm.hs.kr>
2022-04-14 19:40:06 +0900

    ⚡ :: (#29) Invalid User 추가

--- src/main/java/com/example/bj_isfp_backend/global/error/ErrorCode.java
+++ src/main/java/com/example/bj_isfp_backend/global/error/ErrorCode.java
@@ -15,8 +15,7 @@ public enum ErrorCode {
 
     USER_NOT_FOUND(404, "USER-404-1", "User Not Found"),
     USER_NAME_ALREADY_EXISTS(409, "USER-409-1", "User Name Already Exists"),
-
-
+    INVALID_USER(401, "USER-401-1", "Invalid User"),
 
     EXPIRED_JWT(401, "JWT-401-1", "Expired Jwt"),
     INVALID_JWT(401, "JWT-401-2", "Invalid Jwt");
