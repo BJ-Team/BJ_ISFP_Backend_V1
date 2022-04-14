05bf5717f6509b7f7323ee1022dc4169cc8de49c
son <sonjm3518@dsm.hs.kr>
2022-04-14 20:20:43 +0900

    ⚡ :: (#32) Update Place Service 추가

--- src/main/java/com/example/bj_isfp_backend/domain/user/service/UserService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.bj_isfp_backend.domain.auth.presentation.dto.TokenResponse;
 import com.example.bj_isfp_backend.domain.user.presentation.dto.UpdatePasswordRequest;
 import com.example.bj_isfp_backend.domain.user.presentation.dto.LoginRequest;
 import com.example.bj_isfp_backend.domain.user.presentation.dto.SignUpRequest;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.UpdatePlaceRequest;
 
 public interface UserService {
 
@@ -12,4 +13,6 @@ public interface UserService {
     TokenResponse login(LoginRequest loginRequest);
 
     void updatePassword(UpdatePasswordRequest changePasswordRequest);
+
+    void updatePlace(UpdatePlaceRequest updatePlaceRequest);
 }
