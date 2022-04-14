aa46002342ad9cf9b40e74afa565c03b0a671179
son <sonjm3518@dsm.hs.kr>
2022-04-14 19:49:31 +0900

    ⚡ :: (#29) Update Password Service 추가

--- src/main/java/com/example/bj_isfp_backend/domain/user/service/UserService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.bj_isfp_backend.domain.user.service;
 
 import com.example.bj_isfp_backend.domain.auth.presentation.dto.TokenResponse;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.UpdatePasswordRequest;
 import com.example.bj_isfp_backend.domain.user.presentation.dto.LoginRequest;
 import com.example.bj_isfp_backend.domain.user.presentation.dto.SignUpRequest;
 
@@ -9,4 +10,6 @@ public interface UserService {
     void signUp(SignUpRequest signUpRequest);
 
     TokenResponse login(LoginRequest loginRequest);
+
+    void updatePassword(UpdatePasswordRequest changePasswordRequest);
 }
