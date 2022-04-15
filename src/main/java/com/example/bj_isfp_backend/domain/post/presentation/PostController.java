ea7338b0ec526ba2355467f65f56e2016d736e77
son <sonjm3518@dsm.hs.kr>
2022-04-15 11:58:52 +0900

    ⚡ :: delete 추가

--- src/main/java/com/example/bj_isfp_backend/domain/post/presentation/PostController.java
+++ src/main/java/com/example/bj_isfp_backend/domain/post/presentation/PostController.java
@@ -3,9 +3,13 @@ package com.example.bj_isfp_backend.domain.post.presentation;
 import com.example.bj_isfp_backend.domain.post.presentation.dto.CreatePostRequest;
 import com.example.bj_isfp_backend.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -17,8 +21,15 @@ public class PostController {
 
     private final PostService postService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/make")
     public void make(@RequestBody @Valid CreatePostRequest createPostRequest) {
         postService.createPost(createPostRequest);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{post-id}")
+    public void delete(@PathVariable(name = "post-id") Long postId) {
+        postService.deletePost(postId);
     }
 }
