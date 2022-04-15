5bbc9c3b46dfb3224d6f74766be800e10c67aa0d
son <sonjm3518@dsm.hs.kr>
2022-04-15 11:57:26 +0900

    📑 :: (#42) PostNotFound Exception

---
+++ src/main/java/com/example/bj_isfp_backend/domain/post/exception/PostNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.bj_isfp_backend.domain.post.exception;
+
+import com.example.bj_isfp_backend.global.error.BjISFPException;
+import com.example.bj_isfp_backend.global.error.ErrorCode;
+
+public class PostNotFoundException extends BjISFPException {
+    public static PostNotFoundException EXCEPTION =
+            new PostNotFoundException();
+
+    private PostNotFoundException() {
+        super(ErrorCode.POST_NOT_FOUND);
+    }
+}
