fc3933a66d8be35b9182f26ed9637d5a8e2df954
son <sonjm3518@dsm.hs.kr>
2022-04-16 23:03:18 +0900

    📑 :: (#46) Like Repository

---
+++ src/main/java/com/example/bj_isfp_backend/domain/like/domain/repository/LikeRepository.java
@@ -0,0 +1,7 @@
+package com.example.bj_isfp_backend.domain.like.domain.repository;
+
+import com.example.bj_isfp_backend.domain.like.domain.Like;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LikeRepository extends CrudRepository<Like, Long> {
+}
