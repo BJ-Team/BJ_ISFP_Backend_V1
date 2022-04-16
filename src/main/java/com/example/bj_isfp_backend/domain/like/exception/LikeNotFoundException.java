33afd5b2c8e7775081d67598fa4930159c6f9066
son <sonjm3518@dsm.hs.kr>
2022-04-17 01:52:48 +0900

    ⚡ :: ErrorCode에서 받아오기

--- src/main/java/com/example/bj_isfp_backend/domain/like/exception/LikeNotFoundException.java
+++ src/main/java/com/example/bj_isfp_backend/domain/like/exception/LikeNotFoundException.java
@@ -1,12 +1,13 @@
 package com.example.bj_isfp_backend.domain.like.exception;
 
 import com.example.bj_isfp_backend.global.error.BjISFPException;
+import com.example.bj_isfp_backend.global.error.ErrorCode;
 
 public class LikeNotFoundException extends BjISFPException {
     public static LikeNotFoundException EXCEPTION =
             new LikeNotFoundException();
 
     private LikeNotFoundException() {
-        super();
+        super(ErrorCode.LIKE_NOT_FOUND);
     }
 }
