90fb0d960153676ca12c222c8bc285fb9b61e24c
son <sonjm3518@dsm.hs.kr>
2022-04-16 23:03:00 +0900

    📑 :: (#46) Like Entity

---
+++ src/main/java/com/example/bj_isfp_backend/domain/like/domain/Like.java
@@ -0,0 +1,35 @@
+package com.example.bj_isfp_backend.domain.like.domain;
+
+import com.example.bj_isfp_backend.domain.post.domain.Post;
+import com.example.bj_isfp_backend.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Like extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Builder
+    public Like(Post post) {
+        this.post = post;
+    }
+}
