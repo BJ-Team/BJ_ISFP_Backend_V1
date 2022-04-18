df62b989dbd70abd82ceb7b8f6cdb1d1f7988625
son <sonjm3518@dsm.hs.kr>
2022-04-18 23:33:08 +0900

    📑 :: (#58) User Repository Custom

---
+++ src/main/java/com/example/bj_isfp_backend/domain/user/domain/repository/UserRepositoryCustom.java
@@ -0,0 +1,7 @@
+package com.example.bj_isfp_backend.domain.user.domain.repository;
+
+import com.example.bj_isfp_backend.domain.user.domain.repository.vo.UserVO;
+
+public interface UserRepositoryCustom {
+    UserVO queryUserInfo();
+}
