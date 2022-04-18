a9aba5f0ec2f73880fb3a14427cbe358741463e2
son <sonjm3518@dsm.hs.kr>
2022-04-18 23:34:06 +0900

    📑 :: (#58) ProfileService Implement

---
+++ src/main/java/com/example/bj_isfp_backend/domain/user/service/ProfileServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.bj_isfp_backend.domain.user.service;
+
+import com.example.bj_isfp_backend.domain.user.domain.User;
+import com.example.bj_isfp_backend.domain.user.facade.UserFacade;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.response.QueryMyInformationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ProfileServiceImpl implements ProfileService {
+
+    private final UserFacade userFacade;
+
+    @Override
+    @Transactional(readOnly = true)
+    public QueryMyInformationResponse queryMyPage() {
+
+        User user = userFacade.getCurrentUser();
+
+        return QueryMyInformationResponse.builder()
+                .name(user.getName())
+                .nowMyLocation(null)
+                .UserProfile(user.getUserProfile())
+                .build();
+    }
+}
