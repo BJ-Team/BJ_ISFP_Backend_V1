8512713ea715ca17ee97d6fa93d1189224587d68
son <sonjm3518@dsm.hs.kr>
2022-04-18 13:28:27 +0900

    ⚡ :: (#56) queryPostList Service 추가

--- src/main/java/com/example/bj_isfp_backend/domain/post/service/PostService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/post/service/PostService.java
@@ -1,10 +1,13 @@
 package com.example.bj_isfp_backend.domain.post.service;
 
-import com.example.bj_isfp_backend.domain.post.presentation.dto.CreatePostRequest;
+import com.example.bj_isfp_backend.domain.post.presentation.dto.request.CreatePostRequest;
+import com.example.bj_isfp_backend.domain.post.presentation.dto.response.QueryPostResponse;
 
 public interface PostService {
 
     void createPost(CreatePostRequest createPostRequest);
 
     void deletePost(Long postId);
+
+    QueryPostResponse queryPostList();
 }
