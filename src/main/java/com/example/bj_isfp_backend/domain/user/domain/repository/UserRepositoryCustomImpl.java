d86ed16f7d0d5b5e76cfb258f826962d227595c5
son <sonjm3518@dsm.hs.kr>
2022-04-18 23:33:20 +0900

    📑 :: (#58) User Repository Custom Implement

---
+++ src/main/java/com/example/bj_isfp_backend/domain/user/domain/repository/UserRepositoryCustomImpl.java
@@ -0,0 +1,27 @@
+package com.example.bj_isfp_backend.domain.user.domain.repository;
+
+import com.example.bj_isfp_backend.domain.user.domain.repository.vo.QUserVO;
+import com.example.bj_isfp_backend.domain.user.domain.repository.vo.UserVO;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import static com.example.bj_isfp_backend.domain.user.domain.QUser.user;
+
+@RequiredArgsConstructor
+public class UserRepositoryCustomImpl implements UserRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public UserVO queryUserInfo() {
+        return jpaQueryFactory
+                .select(new QUserVO(
+                        user.id,
+                        user.name,
+                        user.userProfile,
+                        user.nowMyLocation
+                ))
+                .from(user)
+                .fetchOne();
+    }
+}
