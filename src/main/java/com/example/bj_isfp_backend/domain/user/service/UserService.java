76a6de7eddf7487c4c387f08682c2c99bec38c41
son <sonjm3518@dsm.hs.kr>
2022-04-18 23:30:54 +0900

    ♻ :: (#58) import 정리

--- src/main/java/com/example/bj_isfp_backend/domain/user/service/UserService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/user/service/UserService.java
@@ -1,11 +1,11 @@
 package com.example.bj_isfp_backend.domain.user.service;
 
 import com.example.bj_isfp_backend.domain.auth.presentation.dto.TokenResponse;
-import com.example.bj_isfp_backend.domain.user.presentation.dto.DuplicateNameCheckRequest;
-import com.example.bj_isfp_backend.domain.user.presentation.dto.LoginRequest;
-import com.example.bj_isfp_backend.domain.user.presentation.dto.SignUpRequest;
-import com.example.bj_isfp_backend.domain.user.presentation.dto.UpdatePasswordRequest;
-import com.example.bj_isfp_backend.domain.user.presentation.dto.UpdatePlaceRequest;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.request.DuplicateNameCheckRequest;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.request.LoginRequest;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.request.SignUpRequest;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.request.UpdatePasswordRequest;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.request.UpdatePlaceRequest;
 
 public interface UserService {
 
