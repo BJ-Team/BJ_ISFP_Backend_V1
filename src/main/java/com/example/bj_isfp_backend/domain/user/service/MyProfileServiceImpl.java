b85e187a7cb56419f0497cc9e3d0684ab0bb2df6
son <sonjm3518@dsm.hs.kr>
2022-04-19 12:09:36 +0900

    ⚡ :: (#60) querySold Service Implement 추가

--- src/main/java/com/example/bj_isfp_backend/domain/user/service/MyProfileServiceImpl.java
+++ src/main/java/com/example/bj_isfp_backend/domain/user/service/MyProfileServiceImpl.java
@@ -1,28 +1,44 @@
 package com.example.bj_isfp_backend.domain.user.service;
 
 import com.example.bj_isfp_backend.domain.user.domain.User;
+import com.example.bj_isfp_backend.domain.user.domain.repository.UserRepository;
 import com.example.bj_isfp_backend.domain.user.facade.UserFacade;
-import com.example.bj_isfp_backend.domain.user.presentation.dto.response.QueryMyInformationResponse;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.response.QueryMyInfoResponse;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.response.QuerySoldResponse;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.response.QuerySoldResponse.SoldResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class MyProfileServiceImpl implements MyProfileService {
 
     private final UserFacade userFacade;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional(readOnly = true)
-    public QueryMyInformationResponse queryMyPage() {
+    public QueryMyInfoResponse queryMyPage() {
 
         User user = userFacade.getCurrentUser();
 
-        return QueryMyInformationResponse.builder()
+        return QueryMyInfoResponse.builder()
                 .name(user.getName())
-                .nowMyLocation(null)
+                .nowMyLocation(user.getNowMyLocation())
                 .UserProfile(user.getUserProfile())
+                .build();
+    }
+
+    @Override
+    public QuerySoldResponse querySold() {
+
+        List<SoldResponse> soldResponseList = userRepository.queryMyPageSoldList();
+
+        return QuerySoldResponse.builder()
+                .soldResponseList(soldResponseList)
                 .build();
     }
 }
