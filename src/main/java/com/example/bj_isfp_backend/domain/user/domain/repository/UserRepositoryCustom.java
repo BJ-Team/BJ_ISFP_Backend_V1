bb0cac7bd07217867eda98c7a0e36063b29597fe
son <sonjm3518@dsm.hs.kr>
2022-04-19 12:11:57 +0900

    ⚡ :: (#60) queryMyPageSoldList 추가

--- src/main/java/com/example/bj_isfp_backend/domain/user/domain/repository/UserRepositoryCustom.java
+++ src/main/java/com/example/bj_isfp_backend/domain/user/domain/repository/UserRepositoryCustom.java
@@ -1,7 +1,13 @@
 package com.example.bj_isfp_backend.domain.user.domain.repository;
 
 import com.example.bj_isfp_backend.domain.user.domain.repository.vo.UserVO;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.response.QuerySoldResponse.SoldResponse;
+
+import java.util.List;
 
 public interface UserRepositoryCustom {
+
     UserVO queryUserInfo();
+
+    List<SoldResponse> queryMyPageSoldList();
 }
