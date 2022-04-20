6af096d15b5f66d4cf075c922cceb387939cd2d2
son <sonjm3518@dsm.hs.kr>
2022-04-20 16:46:42 +0900

    📑 :: (#66) Buy Entity

---
+++ src/main/java/com/example/bj_isfp_backend/domain/buy/domain/Buy.java
@@ -0,0 +1,41 @@
+package com.example.bj_isfp_backend.domain.buy.domain;
+
+import com.example.bj_isfp_backend.domain.post.domain.Post;
+import com.example.bj_isfp_backend.domain.user.domain.User;
+import com.example.bj_isfp_backend.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Buy extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Builder
+    public Buy(User user, Post post) {
+        this.user = user;
+        this.post = post;
+    }
+}
