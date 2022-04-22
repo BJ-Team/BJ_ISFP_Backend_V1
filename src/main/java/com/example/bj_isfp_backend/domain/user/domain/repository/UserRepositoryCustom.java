dfea601d0d0c342ebd955b4b68c9729c1a9207a5
son <sonjm3518@dsm.hs.kr>
2022-04-22 11:54:43 +0900

    ❌ :: Querydsl 삭제

--- src/main/java/com/example/bj_isfp_backend/domain/user/domain/repository/UserRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.example.bj_isfp_backend.domain.user.domain.repository;
-
-import com.example.bj_isfp_backend.domain.user.domain.repository.vo.UserVO;
-
-public interface UserRepositoryCustom {
-
-    UserVO queryMyPageInfo();
-}
