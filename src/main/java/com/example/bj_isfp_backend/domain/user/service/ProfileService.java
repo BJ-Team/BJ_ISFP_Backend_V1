84332e6a0f66326cdd4f78c12f7e9a282b9d9c98
son <sonjm3518@dsm.hs.kr>
2022-04-23 23:09:40 +0900

    ⚡ : (#84) queryUserSold Service 추가

--- src/main/java/com/example/bj_isfp_backend/domain/user/service/ProfileService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/user/service/ProfileService.java
@@ -1,10 +1,11 @@
 package com.example.bj_isfp_backend.domain.user.service;
 
 import com.example.bj_isfp_backend.domain.user.presentation.dto.response.QueryProfileResponse;
+import com.example.bj_isfp_backend.domain.user.presentation.dto.response.QueryUserPostResponse;
 
 public interface ProfileService {
 
     QueryProfileResponse queryProfile(Long userId);
 
-    
+    QueryUserPostResponse queryUserSold(Long userId);
 }
