adfca64622f967703363439907c6110b63a105a3
son <sonjm3518@dsm.hs.kr>
2022-04-24 17:02:50 +0900

    📑 :: (#86) ProfileService

--- src/main/java/com/example/bj_isfp_backend/domain/user/service/ProfileService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/user/service/ProfileService.java
@@ -1,11 +1,27 @@
 package com.example.bj_isfp_backend.domain.user.service;
 
+import com.example.bj_isfp_backend.domain.user.domain.User;
+import com.example.bj_isfp_backend.domain.user.facade.UserFacade;
 import com.example.bj_isfp_backend.domain.user.presentation.dto.response.QueryProfileResponse;
-import com.example.bj_isfp_backend.domain.user.presentation.dto.response.QueryUserPostResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface ProfileService {
+@RequiredArgsConstructor
+@Service
+public class ProfileService {
 
-    QueryProfileResponse queryProfile(Long userId);
+    private final UserFacade userFacade;
 
-    QueryUserPostResponse queryUserSold(Long userId);
+    @Transactional(readOnly = true)
+    public QueryProfileResponse queryProfile(Long userId) {
+
+        User user = userFacade.getUserByUserId(userId);
+
+        return QueryProfileResponse.builder()
+                .userId(user.getId())
+                .name(user.getName())
+                .UserProfile(user.getUserProfile())
+                .build();
+    }
 }
