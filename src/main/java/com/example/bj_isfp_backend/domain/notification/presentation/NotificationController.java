75f00d538f590a52fbc7385459fb5cdb335601d3
son <sonjm3518@dsm.hs.kr>
2022-04-27 23:00:14 +0900

    ⚡ :: (#100) NotificationList 조회 추가

--- src/main/java/com/example/bj_isfp_backend/domain/notification/presentation/NotificationController.java
+++ src/main/java/com/example/bj_isfp_backend/domain/notification/presentation/NotificationController.java
@@ -1,8 +1,10 @@
 package com.example.bj_isfp_backend.domain.notification.presentation;
 
+import com.example.bj_isfp_backend.domain.notification.presentation.dto.NotificationListResponse;
 import com.example.bj_isfp_backend.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +22,10 @@ public class NotificationController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void watchedNotification(@PathVariable(name = "notification-id") Long notificationId) {
         notificationService.WatchedNotification(notificationId);
+    }
+
+    @GetMapping
+    public NotificationListResponse queryNotificationList() {
+        return notificationService.queryNotification();
     }
 }
