2b588ca4781374abf69d5a12ff6a1f6db6e8e19c
son <sonjm3518@dsm.hs.kr>
2022-04-27 23:00:51 +0900

    ⚡ :: (#100) Notification 조회 Service 추가

--- src/main/java/com/example/bj_isfp_backend/domain/notification/service/NotificationService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/notification/service/NotificationService.java
@@ -1,6 +1,10 @@
 package com.example.bj_isfp_backend.domain.notification.service;
 
+import com.example.bj_isfp_backend.domain.notification.presentation.dto.NotificationListResponse;
+
 public interface NotificationService {
 
     void WatchedNotification(Long notificationId);
+
+    NotificationListResponse queryNotification();
 }
