c1bc421c385a397f2ad5e886177ae41803edf8b1
son <sonjm3518@dsm.hs.kr>
2022-04-27 23:01:43 +0900

    ⚡ :: (#100) Notification 조회 Service Implement

--- src/main/java/com/example/bj_isfp_backend/domain/notification/service/NotificationServiceImpl.java
+++ src/main/java/com/example/bj_isfp_backend/domain/notification/service/NotificationServiceImpl.java
@@ -3,14 +3,22 @@ package com.example.bj_isfp_backend.domain.notification.service;
 import com.example.bj_isfp_backend.domain.notification.domain.NotificationEntity;
 import com.example.bj_isfp_backend.domain.notification.domain.repository.NotificationRepository;
 import com.example.bj_isfp_backend.domain.notification.exception.NotificationNotFoundException;
+import com.example.bj_isfp_backend.domain.notification.presentation.dto.NotificationListResponse;
+import com.example.bj_isfp_backend.domain.notification.presentation.dto.NotificationListResponse.NotificationResponse;
+import com.example.bj_isfp_backend.domain.user.domain.User;
+import com.example.bj_isfp_backend.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class NotificationServiceImpl implements NotificationService {
 
+    private final UserFacade userFacade;
     private final NotificationRepository notificationRepository;
 
     @Override
@@ -20,5 +28,27 @@ public class NotificationServiceImpl implements NotificationService {
                 .orElseThrow(() -> NotificationNotFoundException.EXCEPTION);
 
         notification.isWatched();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public NotificationListResponse queryNotification() {
+
+        User user = userFacade.getCurrentUser();
+
+        List<NotificationResponse> notificationResponseList = notificationRepository.findByUser(user)
+                .stream()
+                .map(notificationEntity -> NotificationResponse.builder()
+                        .id(notificationEntity.getId())
+                        .title(notificationEntity.getTitle())
+                        .content(notificationEntity.getContent())
+                        .isWatch(notificationEntity.getIsWatch())
+                        .data(notificationEntity.getData())
+                        .build())
+                .collect(Collectors.toList());
+
+        return NotificationListResponse.builder()
+                .notificationResponseList(notificationResponseList)
+                .build();
     }
 }
