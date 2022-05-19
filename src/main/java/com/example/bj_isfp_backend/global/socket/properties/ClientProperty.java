83dfc7c87b86eb6d42c7e995929f65d60bd9a487
jaemin <sonjm3518@dsm.hs.kr>
2022-05-19 16:08:09 +0900

    📑 :: (#102) Client Property

---
+++ src/main/java/com/example/bj_isfp_backend/global/socket/properties/ClientProperty.java
@@ -0,0 +1,10 @@
+package com.example.bj_isfp_backend.global.socket.properties;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClientProperty {
+
+    public static final String USER_KEY = "user_key";
+}
