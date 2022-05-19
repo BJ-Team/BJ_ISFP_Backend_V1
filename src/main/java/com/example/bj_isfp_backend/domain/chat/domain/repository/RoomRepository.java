b73aec2aee73e9d4de845cb73652c7edd524bdcf
jaemin <sonjm3518@dsm.hs.kr>
2022-05-20 00:08:25 +0900

    📑 :: (#104) Room Repository

---
+++ src/main/java/com/example/bj_isfp_backend/domain/chat/domain/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.example.bj_isfp_backend.domain.chat.domain.repository;
+
+import com.example.bj_isfp_backend.domain.chat.domain.Room;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Long> {
+}
