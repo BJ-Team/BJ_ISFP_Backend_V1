155648e901a4493196d4d0dbe85a8f17364995c6
jaemin <sonjm3518@dsm.hs.kr>
2022-05-19 16:07:56 +0900

    📑 :: (#102) Socket Property

---
+++ src/main/java/com/example/bj_isfp_backend/global/socket/properties/SocketProperty.java
@@ -0,0 +1,12 @@
+package com.example.bj_isfp_backend.global.socket.properties;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SocketProperty {
+
+    public static final String ROOM_KEY = "room_key";
+
+    public static final String ERROR_KEY = "error";
+}
