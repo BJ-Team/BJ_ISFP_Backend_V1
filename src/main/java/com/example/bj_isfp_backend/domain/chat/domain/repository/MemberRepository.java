ab95ab728f031677bfadf5625bc2f0d8e96cc895
jaemin <sonjm3518@dsm.hs.kr>
2022-05-23 09:24:46 +0900

    ⚡ :: (#106) findByUserAndRoom 추가

--- src/main/java/com/example/bj_isfp_backend/domain/chat/domain/repository/MemberRepository.java
+++ src/main/java/com/example/bj_isfp_backend/domain/chat/domain/repository/MemberRepository.java
@@ -1,7 +1,13 @@
 package com.example.bj_isfp_backend.domain.chat.domain.repository;
 
 import com.example.bj_isfp_backend.domain.chat.domain.Member;
+import com.example.bj_isfp_backend.domain.chat.domain.Room;
+import com.example.bj_isfp_backend.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends CrudRepository<Member, Long> {
+
+    Optional<Member> findByUserAndRoom(User user, Room room);
 }
