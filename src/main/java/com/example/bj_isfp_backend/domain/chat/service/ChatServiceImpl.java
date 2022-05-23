444d8fcddde06156ffe6be70bb06bf72eff45a50
jaemin <sonjm3518@dsm.hs.kr>
2022-05-23 16:07:27 +0900

    ⚡ :: (#109) queryMessage Service Implement

--- src/main/java/com/example/bj_isfp_backend/domain/chat/service/ChatServiceImpl.java
+++ src/main/java/com/example/bj_isfp_backend/domain/chat/service/ChatServiceImpl.java
@@ -7,9 +7,16 @@ import com.example.bj_isfp_backend.domain.chat.domain.repository.MessageReposito
 import com.example.bj_isfp_backend.domain.chat.facade.MemberFacade;
 import com.example.bj_isfp_backend.domain.chat.facade.RoomFacade;
 import com.example.bj_isfp_backend.domain.chat.presentation.dto.request.ChatRequest;
+import com.example.bj_isfp_backend.domain.chat.presentation.dto.response.MessageListResponse;
+import com.example.bj_isfp_backend.domain.chat.presentation.dto.response.MessageListResponse.MessageResponse;
 import com.example.bj_isfp_backend.domain.user.domain.User;
+import com.example.bj_isfp_backend.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -18,9 +25,11 @@ public class ChatServiceImpl implements ChatService {
 
     private final RoomFacade roomFacade;
     private final MemberFacade memberFacade;
+    private final UserFacade userFacade;
     private final MessageRepository messageRepository;
 
     @Override
+    @Transactional
     public void saveMessage(ChatRequest chatRequest, User user) {
 
         Room room = roomFacade.getRoomId(chatRequest.getRoomId());
@@ -32,5 +41,34 @@ public class ChatServiceImpl implements ChatService {
                         .room(room)
                         .member(member)
                         .build());
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public MessageListResponse queryMessage(Long roomId) {
+
+        Room room = roomFacade.getRoomId(roomId);
+        User user = userFacade.getCurrentUser();
+
+        memberFacade.isEmptyMessageByUserAndRoom(user, room);
+
+        List<MessageResponse> messageResponseList = messageRepository.findByRoom(room)
+                .stream()
+                .map(this::messageBuilder)
+                .collect(Collectors.toList());
+
+        return MessageListResponse.builder()
+                .messageResponseList(messageResponseList)
+                .build();
+    }
+
+    private MessageResponse messageBuilder(Message message) {
+        return MessageResponse.builder()
+                .messageId(message.getId())
+                .content(message.getContent())
+                .createTime(message.getCreateTime())
+                .member(message.getMember())
+                .room(message.getRoom())
+                .build();
     }
 }
