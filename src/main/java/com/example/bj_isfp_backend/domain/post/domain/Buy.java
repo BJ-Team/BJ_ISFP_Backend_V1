888f044655c23d7af7f4a3bfbdcafdf1bf830384
jaemin <sonjm3518@dsm.hs.kr>
2022-05-24 23:08:49 +0900

    ♻ :: 쓸데없는 코드 삭제, 리팩토링

--- src/main/java/com/example/bj_isfp_backend/domain/post/domain/Buy.java
+++ src/main/java/com/example/bj_isfp_backend/domain/post/domain/Buy.java
@@ -1,6 +1,5 @@
 package com.example.bj_isfp_backend.domain.post.domain;
 
-import com.example.bj_isfp_backend.domain.post.domain.Post;
 import com.example.bj_isfp_backend.domain.user.domain.User;
 import com.example.bj_isfp_backend.global.entity.BaseTimeEntity;
 import lombok.AccessLevel;
@@ -8,13 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
--- src/main/java/com/example/bj_isfp_backend/domain/post/presentation/PostController.java
+++ src/main/java/com/example/bj_isfp_backend/domain/post/presentation/PostController.java
@@ -5,14 +5,7 @@ import com.example.bj_isfp_backend.domain.post.presentation.dto.response.QueryPo
 import com.example.bj_isfp_backend.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
