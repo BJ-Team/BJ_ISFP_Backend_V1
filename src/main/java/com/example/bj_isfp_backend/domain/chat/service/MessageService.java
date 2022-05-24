c65fbb48d5072e838125a808b86656e6503bd470
jaemin <sonjm3518@dsm.hs.kr>
2022-05-24 23:02:28 +0900

    ⚡ :: (#107) Message로 변환타입 바꾸기

--- src/main/java/com/example/bj_isfp_backend/domain/chat/service/MessageService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/chat/service/MessageService.java
@@ -1,11 +1,12 @@
 package com.example.bj_isfp_backend.domain.chat.service;
 
+import com.example.bj_isfp_backend.domain.chat.domain.Message;
 import com.example.bj_isfp_backend.domain.chat.presentation.dto.request.ChatRequest;
 import com.example.bj_isfp_backend.domain.chat.presentation.dto.response.MessageListResponse;
 import com.example.bj_isfp_backend.domain.user.domain.User;
 
-public interface ChatService {
-    void saveMessage(ChatRequest chatRequest, User user);
+public interface MessageService {
+    Message saveMessage(ChatRequest chatRequest, User user);
 
     MessageListResponse queryMessage(Long roomId);
 }
