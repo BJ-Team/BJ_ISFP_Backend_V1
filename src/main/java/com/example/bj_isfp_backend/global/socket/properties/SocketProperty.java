2bc39a3f58eb8026dabd025397d54d83462094c2
jaemin <sonjm3518@dsm.hs.kr>
2022-05-24 23:09:03 +0900

    ⚡ :: Message_Key 추가

--- src/main/java/com/example/bj_isfp_backend/global/socket/properties/SocketProperty.java
+++ src/main/java/com/example/bj_isfp_backend/global/socket/properties/SocketProperty.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SocketProperty {
 
+    public static final String MESSAGE_KEY = "message_key";
+
     public static final String ROOM_KEY = "room_key";
 
     public static final String ERROR_KEY = "error";
