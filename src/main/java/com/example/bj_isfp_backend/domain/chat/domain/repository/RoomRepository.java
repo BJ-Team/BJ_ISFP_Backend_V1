5cbf928fdd90fd7c103129c54023b36e402b31b5
jaemin <sonjm3518@dsm.hs.kr>
2022-05-25 11:02:29 +0900

    ⚡ :: (#112) findByUserAndpost

--- src/main/java/com/example/bj_isfp_backend/domain/chat/domain/repository/RoomRepository.java
+++ src/main/java/com/example/bj_isfp_backend/domain/chat/domain/repository/RoomRepository.java
@@ -1,7 +1,12 @@
 package com.example.bj_isfp_backend.domain.chat.domain.repository;
 
 import com.example.bj_isfp_backend.domain.chat.domain.Room;
+import com.example.bj_isfp_backend.domain.post.domain.Post;
+import com.example.bj_isfp_backend.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RoomRepository extends CrudRepository<Room, Long> {
+    Optional<Room> findByUserAndPost(User user, Post post);
 }
