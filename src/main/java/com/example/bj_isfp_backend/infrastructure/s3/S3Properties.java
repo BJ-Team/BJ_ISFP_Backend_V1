39f7feb4e23cdf19cd373ce528c44bb5f1852230
jaemin <sonjm3518@dsm.hs.kr>
2022-05-25 16:44:28 +0900

    ⚡ :: @RequiredArgsConstructor

--- src/main/java/com/example/bj_isfp_backend/infrastructure/s3/S3Properties.java
+++ src/main/java/com/example/bj_isfp_backend/infrastructure/s3/S3Properties.java
@@ -1,10 +1,12 @@
 package com.example.bj_isfp_backend.infrastructure.s3;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
+@RequiredArgsConstructor
 @ConstructorBinding
 @ConfigurationProperties(prefix = "aws.s3")
 public class S3Properties {
@@ -13,11 +15,4 @@ public class S3Properties {
     private final String iamSecretKey;
     private final String bucket;
     private final String iamRegion;
-
-    public S3Properties(String iamAccessKey, String iamSecretKey, String bucket, String iamRegion) {
-        this.iamAccessKey = iamAccessKey;
-        this.iamSecretKey = iamSecretKey;
-        this.bucket = bucket;
-        this.iamRegion = iamRegion;
-    }
 }
