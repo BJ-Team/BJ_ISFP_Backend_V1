97df5d6229f84dd562060e7a9f24de7c1756a5d8
jaemin <sonjm3518@dsm.hs.kr>
2022-08-04 21:44:37 +0900

    ⚡ :: updatePost Service

--- src/main/java/com/example/bj_isfp_backend/domain/post/service/PostService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/post/service/PostService.java
@@ -1,13 +1,15 @@
 package com.example.bj_isfp_backend.domain.post.service;
 
-import com.example.bj_isfp_backend.domain.post.presentation.dto.request.CreatePostRequest;
+import com.example.bj_isfp_backend.domain.post.presentation.dto.request.PostRequest;
 import com.example.bj_isfp_backend.domain.post.presentation.dto.response.QueryPostResponse;
 
 public interface PostService {
 
-    void createPost(CreatePostRequest createPostRequest);
+    void createPost(PostRequest postRequest);
 
     void deletePost(Long postId);
 
     QueryPostResponse queryPostList();
+
+    void updatePost(Long postId, PostRequest postRequest);
 }
