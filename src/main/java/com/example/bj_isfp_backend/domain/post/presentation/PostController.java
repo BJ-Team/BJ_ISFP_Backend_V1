ad90df75788e58a0891c8f68620453088906e1d6
jaemin <sonjm3518@dsm.hs.kr>
2022-08-04 21:44:47 +0900

    ⚡ :: updatePost Controller

--- src/main/java/com/example/bj_isfp_backend/domain/post/presentation/PostController.java
+++ src/main/java/com/example/bj_isfp_backend/domain/post/presentation/PostController.java
@@ -1,6 +1,6 @@
 package com.example.bj_isfp_backend.domain.post.presentation;
 
-import com.example.bj_isfp_backend.domain.post.presentation.dto.request.CreatePostRequest;
+import com.example.bj_isfp_backend.domain.post.presentation.dto.request.PostRequest;
 import com.example.bj_isfp_backend.domain.post.presentation.dto.response.QueryPostResponse;
 import com.example.bj_isfp_backend.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -18,18 +18,23 @@ public class PostController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/make")
-    public void make(@RequestBody @Valid CreatePostRequest createPostRequest) {
-        postService.createPost(createPostRequest);
+    public void makePost(@RequestBody @Valid PostRequest postRequest) {
+        postService.createPost(postRequest);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{post-id}")
-    public void delete(@PathVariable(name = "post-id") Long postId) {
+    public void deletePost(@PathVariable(name = "post-id") Long postId) {
         postService.deletePost(postId);
     }
 
     @GetMapping
     public QueryPostResponse queryPost() {
         return postService.queryPostList();
+    }
+
+    @PatchMapping("/{post-id}")
+    public void updatePost(@PathVariable(name = "post-id") Long postId, PostRequest postRequest) {
+        postService.updatePost(postId, postRequest);
     }
 }
