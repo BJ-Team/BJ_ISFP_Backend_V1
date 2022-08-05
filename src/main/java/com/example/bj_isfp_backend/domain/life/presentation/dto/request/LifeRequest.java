81902ec1912ef2a3d2212a39e9e7e766a898e945
jaemin <sonjm3518@dsm.hs.kr>
2022-08-05 18:27:51 +0900

    📑 :: Life Request

---
+++ src/main/java/com/example/bj_isfp_backend/domain/life/presentation/dto/request/LifeRequest.java
@@ -0,0 +1,23 @@
+package com.example.bj_isfp_backend.domain.life.presentation.dto.request;
+
+import com.example.bj_isfp_backend.domain.life.domain.type.LifeCategory;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class LifeRequest {
+
+    @Size(max = 300, message = "title은 300자 이내로 입력해주세요.")
+    @NotBlank(message = "content 필수 입력입니다.")
+    private String content;
+
+    @NotNull(message = "category는 필수 입력입니다.")
+    private LifeCategory category;
+
+    private String lifeImage;
+}
