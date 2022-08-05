0a022ccbf051606edec42b579c90fb3fbedf2e46
jaemin <sonjm3518@dsm.hs.kr>
2022-08-05 18:27:08 +0900

    📑 :: Life Repository Custom Implement

---
+++ src/main/java/com/example/bj_isfp_backend/domain/life/domain/repository/LifeRepositoryCustomImpl.java
@@ -0,0 +1,32 @@
+package com.example.bj_isfp_backend.domain.life.domain.repository;
+
+import com.example.bj_isfp_backend.domain.life.presentation.dto.response.QQueryLifeResponse_LifeResponse;
+import com.example.bj_isfp_backend.domain.life.presentation.dto.response.QueryLifeResponse.LifeResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.example.bj_isfp_backend.domain.life.domain.QLife.life;
+import static com.example.bj_isfp_backend.domain.user.domain.QUser.user;
+
+@RequiredArgsConstructor
+public class LifeRepositoryCustomImpl implements LifeRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<LifeResponse> queryLifeList() {
+        return jpaQueryFactory
+                .select(new QQueryLifeResponse_LifeResponse(
+                        life.content,
+                        life.category,
+                        life.createTime,
+                        life.lifeImage,
+                        user.id
+                ))
+                .from(life)
+                .join(user)
+                .fetch();
+    }
+}
