603f890aa7b60bb61daa5b407868834e9731fcba
jaemin <sonjm3518@dsm.hs.kr>
2022-08-07 19:19:13 +0900

    ⚡ :: queryPostDetails Service

--- src/main/java/com/example/bj_isfp_backend/domain/post/service/PostService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/post/service/PostService.java
@@ -1,5 +1,6 @@
 package com.example.bj_isfp_backend.domain.post.service;
 
+import com.example.bj_isfp_backend.domain.post.domain.repository.vo.PostDetailsVO;
 import com.example.bj_isfp_backend.domain.post.domain.repository.vo.PostListVO;
 import com.example.bj_isfp_backend.domain.post.presentation.dto.request.PostRequest;
 
@@ -11,6 +12,6 @@ public interface PostService {
 
     PostListVO queryPostList();
 
-
+    PostDetailsVO queryPostDetails(Long postId);
     void updatePost(Long postId, PostRequest postRequest);
 }
