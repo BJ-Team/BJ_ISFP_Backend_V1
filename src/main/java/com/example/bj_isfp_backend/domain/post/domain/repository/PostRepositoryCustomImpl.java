4e18bedd563883034f58ccd0fc23e93d03ac433a
jaemin <sonjm3518@dsm.hs.kr>
2022-08-07 19:13:50 +0900

    ⚡ :: queryPostDetails 구현

--- src/main/java/com/example/bj_isfp_backend/domain/post/domain/repository/PostRepositoryCustomImpl.java
+++ src/main/java/com/example/bj_isfp_backend/domain/post/domain/repository/PostRepositoryCustomImpl.java
@@ -1,7 +1,12 @@
 package com.example.bj_isfp_backend.domain.post.domain.repository;
 
-import com.example.bj_isfp_backend.domain.post.presentation.dto.response.QQueryPostResponse_PostResponse;
-import com.example.bj_isfp_backend.domain.post.presentation.dto.response.QueryPostResponse.PostResponse;
+
+import com.example.bj_isfp_backend.domain.post.domain.repository.vo.PostDetailsVO;
+import com.example.bj_isfp_backend.domain.post.domain.repository.vo.PostListVO.PostVO;
+import com.example.bj_isfp_backend.domain.post.domain.repository.vo.QPostDetailsVO;
+import com.example.bj_isfp_backend.domain.post.domain.repository.vo.QPostListVO_PostVO;
+import com.example.bj_isfp_backend.domain.user.domain.User;
+import com.example.bj_isfp_backend.domain.user.facade.UserFacade;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -10,25 +15,54 @@ import java.util.List;
 import static com.example.bj_isfp_backend.domain.post.domain.QPost.post;
 import static com.example.bj_isfp_backend.domain.user.domain.QUser.user;
 
-
 @RequiredArgsConstructor
 public class PostRepositoryCustomImpl implements PostRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
+    private final UserFacade userFacade;
 
     @Override
-    public List<PostResponse> queryPostList() {
-        return jpaQueryFactory
-                .select(new QQueryPostResponse_PostResponse(
-                        post.title,
-                        user.location,
-                        post.createTime,
-                        post.price,
-                        post.postImage,
-                        post.id
-                ))
+    public List<PostVO> queryPostList() {
+        User currentUser = userFacade.getCurrentUser();
+
+        return jpaQueryFactory.select(
+                        new QPostListVO_PostVO(
+                                post.title,
+                                post.location,
+                                post.isReported,
+                                post.createTime,
+                                post.price,
+                                post.postImage,
+                                post.id
+                        ))
                 .from(post)
-                .join(post.user, user)
+                .where(
+                        post.location.eq(currentUser.getLocation())
+                )
                 .fetch();
+    }
+
+    @Override
+    public PostDetailsVO queryPostDetails(Long postId) {
+        return jpaQueryFactory.select(
+                        new QPostDetailsVO(
+                                post.title,
+                                post.content,
+                                post.category,
+                                post.location,
+                                post.createTime,
+                                post.isReported,
+                                post.price,
+                                post.postImage,
+                                user.name,
+                                user.userProfile,
+                                user.id
+                        ))
+                .from(post)
+                .leftJoin(post.user, user)
+                .where(
+                        post.id.eq(postId)
+                )
+                .fetchOne();
     }
 }
