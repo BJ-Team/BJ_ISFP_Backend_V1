a61ca4c376822f53382cf584082e7a1caed3e135
jaemin <sonjm3518@dsm.hs.kr>
2022-08-07 19:13:28 +0900

    ⚡ :: queryPostDetails 추가

--- src/main/java/com/example/bj_isfp_backend/domain/post/domain/repository/PostRepositoryCustom.java
+++ src/main/java/com/example/bj_isfp_backend/domain/post/domain/repository/PostRepositoryCustom.java
@@ -1,9 +1,12 @@
 package com.example.bj_isfp_backend.domain.post.domain.repository;
 
-import com.example.bj_isfp_backend.domain.post.presentation.dto.response.QueryPostResponse.PostResponse;
+import com.example.bj_isfp_backend.domain.post.domain.repository.vo.PostDetailsVO;
+import com.example.bj_isfp_backend.domain.post.domain.repository.vo.PostListVO.PostVO;
 
 import java.util.List;
 
 public interface PostRepositoryCustom {
-    List<PostResponse> queryPostList();
+    List<PostVO> queryPostList();
+
+    PostDetailsVO queryPostDetails(Long postId);
 }
