ef585e04b12a13c5115d469a2a14319f24f20959
jaemin <sonjm3518@dsm.hs.kr>
2022-08-14 00:50:37 +0900

    ⚡ :: queryLifeDetails 추가

--- src/main/java/com/example/bj_isfp_backend/domain/life/domain/repository/LifeRepositoryCustom.java
+++ src/main/java/com/example/bj_isfp_backend/domain/life/domain/repository/LifeRepositoryCustom.java
@@ -1,10 +1,13 @@
 package com.example.bj_isfp_backend.domain.life.domain.repository;
 
-import com.example.bj_isfp_backend.domain.life.presentation.dto.response.QueryLifeResponse.LifeResponse;
+import com.example.bj_isfp_backend.domain.life.domain.repository.vo.LifeDetailsVO.LifeVO;
+import com.example.bj_isfp_backend.domain.life.domain.repository.vo.LifeListVO.LifeResponse;
 
 import java.util.List;
 
 public interface LifeRepositoryCustom {
 
     List<LifeResponse> queryLifeList();
+
+    LifeVO queryLifeDetails(Long lifeId);
 }
