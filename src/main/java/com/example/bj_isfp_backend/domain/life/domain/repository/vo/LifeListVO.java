0d550b8fcf01192aa9243a9bea3d8cae6218b28e
jaemin <sonjm3518@dsm.hs.kr>
2022-08-14 00:55:15 +0900

    ♻️ :: location 추가

--- src/main/java/com/example/bj_isfp_backend/domain/life/domain/repository/vo/LifeListVO.java
+++ src/main/java/com/example/bj_isfp_backend/domain/life/domain/repository/vo/LifeListVO.java
@@ -1,6 +1,7 @@
-package com.example.bj_isfp_backend.domain.life.presentation.dto.response;
+package com.example.bj_isfp_backend.domain.life.domain.repository.vo;
 
 import com.example.bj_isfp_backend.domain.life.domain.type.LifeCategory;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,24 +11,26 @@ import java.util.List;
 
 @Getter
 @Builder
-public class QueryLifeResponse {
-
+public class LifeListVO {
     private final List<LifeResponse> lifeResponses;
 
     @Getter
     public static class LifeResponse {
         private final String content;
-
         private final LifeCategory category;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
         private final LocalDateTime createTime;
+        private final String location;
         private final String lifeImage;
         private final Long lifeId;
 
         @QueryProjection
-        public LifeResponse(String content, LifeCategory category, LocalDateTime createTime, String lifeImage, Long lifeId) {
+        public LifeResponse(String content, LifeCategory category, LocalDateTime createTime, String location, String lifeImage, Long lifeId) {
             this.content = content;
             this.category = category;
             this.createTime = createTime;
+            this.location = location;
             this.lifeImage = lifeImage;
             this.lifeId = lifeId;
         }
