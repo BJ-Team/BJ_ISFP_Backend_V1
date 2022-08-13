b878fb869197ecc03c67c8eca44a75d7a8a2a59a
jaemin <sonjm3518@dsm.hs.kr>
2022-08-14 00:52:52 +0900

    ♻️ :: JsonFormat 적용

--- src/main/java/com/example/bj_isfp_backend/domain/post/domain/repository/vo/PostListVO.java
+++ src/main/java/com/example/bj_isfp_backend/domain/post/domain/repository/vo/PostListVO.java
@@ -1,5 +1,6 @@
 package com.example.bj_isfp_backend.domain.post.domain.repository.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,20 +11,22 @@ import java.util.List;
 @Getter
 @Builder
 public class PostListVO {
-    private final List<PostVO> postVO;
+    private final List<PostResponse> postList;
 
     @Getter
-    public static class PostVO {
+    public static class PostResponse {
         private final String title;
         private final String location;
         private final boolean isLiked;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
         private final LocalDateTime createTime;
         private final Integer price;
         private final String postImage;
         private final Long postId;
 
         @QueryProjection
-        public PostVO(String title, String location, boolean isLiked, LocalDateTime createTime, Integer price, String postImage, Long postId) {
+        public PostResponse(String title, String location, boolean isLiked, LocalDateTime createTime, Integer price, String postImage, Long postId) {
             this.title = title;
             this.location = location;
             this.isLiked = isLiked;
