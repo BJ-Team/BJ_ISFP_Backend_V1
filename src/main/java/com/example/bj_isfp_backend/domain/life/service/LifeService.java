fb9326ab11f9037ae297f53e1797d043b9cefecc
jaemin <sonjm3518@dsm.hs.kr>
2022-08-14 00:51:11 +0900

    ⚡ :: queryLifeDetails Service

--- src/main/java/com/example/bj_isfp_backend/domain/life/service/LifeService.java
+++ src/main/java/com/example/bj_isfp_backend/domain/life/service/LifeService.java
@@ -1,14 +1,17 @@
 package com.example.bj_isfp_backend.domain.life.service;
 
+import com.example.bj_isfp_backend.domain.life.domain.repository.vo.LifeDetailsVO.LifeVO;
+import com.example.bj_isfp_backend.domain.life.domain.repository.vo.LifeListVO;
 import com.example.bj_isfp_backend.domain.life.presentation.dto.request.LifeRequest;
-import com.example.bj_isfp_backend.domain.life.presentation.dto.response.QueryLifeResponse;
 
 public interface LifeService {
     void createLife(LifeRequest lifeRequest);
 
     void updateLife(Long lifeId, LifeRequest lifeRequest);
 
-    QueryLifeResponse queryLife();
+    LifeListVO queryLife();
+
+    LifeVO queryLifeDetails(Long lifeId);
 
     void deleteLife(Long lifeId);
 }
