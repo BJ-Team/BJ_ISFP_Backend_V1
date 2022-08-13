5b66793e665507b7ff8fb2c3ed41b94b74f05fb8
jaemin <sonjm3518@dsm.hs.kr>
2022-08-14 00:51:25 +0900

    ⚡ :: queryLifeDetails Service Implement

--- src/main/java/com/example/bj_isfp_backend/domain/life/service/LifeServiceImpl.java
+++ src/main/java/com/example/bj_isfp_backend/domain/life/service/LifeServiceImpl.java
@@ -2,9 +2,10 @@ package com.example.bj_isfp_backend.domain.life.service;
 
 import com.example.bj_isfp_backend.domain.life.domain.Life;
 import com.example.bj_isfp_backend.domain.life.domain.repository.LifeRepository;
+import com.example.bj_isfp_backend.domain.life.domain.repository.vo.LifeDetailsVO.LifeVO;
 import com.example.bj_isfp_backend.domain.life.domain.repository.vo.LifeListVO;
-import com.example.bj_isfp_backend.domain.life.domain.repository.vo.LifeListVO.LifeVO;
-import com.example.bj_isfp_backend.domain.life.exception.LifeNotFoundException;
+import com.example.bj_isfp_backend.domain.life.domain.repository.vo.LifeListVO.LifeResponse;
+import com.example.bj_isfp_backend.domain.life.facade.LifeFacade;
 import com.example.bj_isfp_backend.domain.life.presentation.dto.request.LifeRequest;
 import com.example.bj_isfp_backend.domain.user.domain.User;
 import com.example.bj_isfp_backend.domain.user.exception.InvalidUserException;
@@ -20,6 +21,7 @@ import java.util.List;
 public class LifeServiceImpl implements LifeService {
 
     private final UserFacade userFacade;
+    private final LifeFacade lifeFacade;
     private final LifeRepository lifeRepository;
 
     @Override
@@ -41,10 +43,9 @@ public class LifeServiceImpl implements LifeService {
 
     @Override
     public void updateLife(Long lifeId, LifeRequest lifeRequest) {
-        User user = userFacade.getCurrentUser();
 
-        Life life = lifeRepository.findById(lifeId)
-                .orElseThrow(() -> LifeNotFoundException.EXCEPTION);
+        User user = userFacade.getCurrentUser();
+        Life life = lifeFacade.getLifeById(lifeId);
 
         if (!life.getUser().equals(user))
             throw InvalidUserException.EXCEPTION;
@@ -58,7 +59,7 @@ public class LifeServiceImpl implements LifeService {
 
     @Override
     public LifeListVO queryLife() {
-        List<LifeVO> lifeList = lifeRepository.queryLifeList();
+        List<LifeResponse> lifeList = lifeRepository.queryLifeList();
 
         return LifeListVO.builder()
                 .lifeResponses(lifeList)
@@ -66,13 +67,16 @@ public class LifeServiceImpl implements LifeService {
     }
 
     @Override
+    public LifeVO queryLifeDetails(Long lifeId) {
+        return lifeRepository.queryLifeDetails(lifeId);
+    }
+
+    @Override
     @Transactional
     public void deleteLife(Long lifeId) {
 
         User user = userFacade.getCurrentUser();
-
-        Life life = lifeRepository.findById(lifeId)
-                .orElseThrow(() -> LifeNotFoundException.EXCEPTION);
+        Life life = lifeFacade.getLifeById(lifeId);
 
         if (!life.getUser().equals(user))
             throw InvalidUserException.EXCEPTION;
