fab813fa9dab1cb422f0338eaa5a829ece9cf92b
jaemin <sonjm3518@dsm.hs.kr>
2022-08-13 23:56:20 +0900

    ⚡ :: Comment 추가

--- src/main/java/com/example/bj_isfp_backend/domain/life/domain/Life.java
+++ src/main/java/com/example/bj_isfp_backend/domain/life/domain/Life.java
@@ -1,5 +1,6 @@
 package com.example.bj_isfp_backend.domain.life.domain;
 
+import com.example.bj_isfp_backend.domain.commment.domain.Comment;
 import com.example.bj_isfp_backend.domain.life.domain.type.LifeCategory;
 import com.example.bj_isfp_backend.domain.user.domain.User;
 import com.example.bj_isfp_backend.global.entity.BaseTimeEntity;
@@ -7,8 +8,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,28 +22,38 @@ public class Life extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 300, nullable = false)
+    @NotNull
+    @Length(max = 300)
     private String content;
 
     @Column(nullable = false)
     private LifeCategory category;
+
+    private String location;
     private String lifeImage;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
     @Builder
-    public Life(String content, LifeCategory category, String lifeImage, User user) {
+    public Life(String content, LifeCategory category, String location, String lifeImage, User user, Comment comment) {
         this.content = content;
         this.category = category;
-        this.user = user;
+        this.location = location;
         if (lifeImage != null) this.lifeImage = lifeImage;
+        this.user = user;
+        this.comment = comment;
     }
 
-    public void updateLife(String content, LifeCategory category, String lifeImage) {
+    public void updateLife(String content, LifeCategory category, String location, String lifeImage) {
         this.content = content;
         this.category = category;
+        this.location = location;
         if (lifeImage != null) this.lifeImage = lifeImage;
     }
 }
