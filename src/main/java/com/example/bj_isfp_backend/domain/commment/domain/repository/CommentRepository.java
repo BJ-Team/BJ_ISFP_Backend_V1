af2a6ec13c35039dc925805d7ac60cb04727ee18
jaemin <sonjm3518@dsm.hs.kr>
2022-08-16 03:00:42 +0900

    ⚡ :: findAllByLife 추가

--- src/main/java/com/example/bj_isfp_backend/domain/commment/domain/repository/CommentRepository.java
+++ src/main/java/com/example/bj_isfp_backend/domain/commment/domain/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.example.bj_isfp_backend.domain.commment.domain.repository;
 
 import com.example.bj_isfp_backend.domain.commment.domain.Comment;
+import com.example.bj_isfp_backend.domain.life.domain.Life;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CommentRepository extends CrudRepository<Comment, Long> {
+import java.util.Optional;
+
+public interface CommentRepository extends CrudRepository<Comment, Long>, CommentRepositoryCustom {
+    Optional<Comment> findAllByLife(Life life);
 }
